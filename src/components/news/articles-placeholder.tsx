66be91ee1c9abc6ecb11a7f17b5254240e8bc7cd
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-11-09 17:00:19 -0500

    update grid placeholder

--- src/components/news/articles-placeholder.tsx
+++ src/components/news/articles-placeholder.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ArticlesPlaceholder = () => {
   return (
-    <div className="grid md:grid-cols-3 gap-4">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {Array.from({ length: 20 }).map((_, idx) => (
         <div key={idx} className="bg-gray-800 min-h-[200px] animate-pulse"></div>
       ))}
